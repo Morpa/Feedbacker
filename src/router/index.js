a1798b26bc21a3fe098034e85e9355718f232aca
Morpa <andremorpa@gmail.com>
2022-01-12 20:35:55 +0000

    Initial configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+const Home = () => import("../views/Home/index.vue");
+const Feedbacks = () => import("../views/Feedbacks/index.vue");
+const Credentials = () => import("../views/Credentials/index.vue");
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/feedbacks",
+    name: "Feedbacks",
+    component: Feedbacks,
+    meta: {
+      hasAuth: true,
+    },
+  },
+  {
+    path: "/credentials",
+    name: "Credentials",
+    component: Credentials,
+    meta: {
+      hasAuth: true,
+    },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: { name: "Home" },
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHistory("/"),
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Feedbacks/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Aqui é a Feedback</h1>
+</template>
